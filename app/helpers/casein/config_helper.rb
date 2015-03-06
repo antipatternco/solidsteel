b4c4b422f94621d8c52770b22b934b2423864e8a
Paul Carvill <paul@paulcarvill.com>
2015-03-06 00:17:16 +0000

    changed cms admin email from address. fixed prod env config so invite email gets sent (using sendmail)

--- app/helpers/casein/config_helper.rb
+++ app/helpers/casein/config_helper.rb
@@ -14,7 +14,7 @@ module Casein
     # The server hostname where Casein will run
     def casein_config_hostname
       if Rails.env.production?
-        'http://178.62.43.232'
+        'http://178.62.62.19'
       else
         'http://0.0.0.0:300'
       end
@@ -22,7 +22,7 @@ module Casein
 
     # The sender address used for email notifications
     def casein_config_email_from_address
-      'cms@solidsteel.com'
+      'solidsteelwebsite@solidsteel.net'
     end
   
     # The initial page the user is shown after they sign in or click the logo. Probably this should be set to the first tab.
