afeaf471a8aaab307952da6fa5dd3b20e06d5a58
Paul Carvill <paul@paulcarvill.com>
2015-03-30 15:00:21 +0100

    added broadcast bg img

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328164307) do
+ActiveRecord::Schema.define(version: 20150330135922) do
 
   create_table "broadcasts", force: :cascade do |t|
-    t.text     "session_name",       limit: 65535
+    t.text     "session_name",                  limit: 65535
     t.datetime "broadcast_date"
-    t.text     "notes",              limit: 65535
-    t.text     "altnotes",           limit: 65535
-    t.text     "link1",              limit: 65535
-    t.text     "link2",              limit: 65535
-    t.text     "link3",              limit: 65535
-    t.text     "link4",              limit: 65535
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.string   "image_file_name",    limit: 255
-    t.string   "image_content_type", limit: 255
-    t.integer  "image_file_size",    limit: 4
+    t.text     "notes",                         limit: 65535
+    t.text     "altnotes",                      limit: 65535
+    t.text     "link1",                         limit: 65535
+    t.text     "link2",                         limit: 65535
+    t.text     "link3",                         limit: 65535
+    t.text     "link4",                         limit: 65535
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "image_file_name",               limit: 255
+    t.string   "image_content_type",            limit: 255
+    t.integer  "image_file_size",               limit: 4
     t.datetime "image_updated_at"
-    t.boolean  "featured",           limit: 1
-    t.boolean  "highlight",          limit: 1
+    t.boolean  "featured",                      limit: 1
+    t.boolean  "highlight",                     limit: 1
+    t.string   "background_image_file_name",    limit: 255
+    t.string   "background_image_content_type", limit: 255
+    t.integer  "background_image_file_size",    limit: 4
+    t.datetime "background_image_updated_at"
   end
 
   create_table "casein_admin_users", force: :cascade do |t|
