5e153085cd096080e36cec1494dd32e812f915cb
Paul Carvill <paul@paulcarvill.com>
2015-04-03 12:22:16 +0100

    adding mix notes to the API endpoint for broadcast

--- app/serializers/simple_broadcast_serializer.rb
+++ app/serializers/simple_broadcast_serializer.rb
@@ -1,3 +1,3 @@
 class SimpleBroadcastSerializer < ActiveModel::Serializer
-  attributes :id, :session_name, :broadcast_date, :image, :background_image, :highlight
+  attributes :id, :session_name, :broadcast_date, :image, :altnotes, :background_image, :highlight
 end
