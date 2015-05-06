a08ca8035768486249d1a09967c032bf34b949bf
Paul Carvill <paul@paulcarvill.com>
2015-05-06 20:21:34 +0100

    displaying playcounts

--- app/serializers/mix_serializer.rb
+++ app/serializers/mix_serializer.rb
@@ -1,4 +1,4 @@
 class MixSerializer < ActiveModel::Serializer
-  attributes :id, :name, :part, :soundcloudId, :background_image, :mix_image, :secret
+  attributes :id, :name, :part, :soundcloudId, :background_image, :mix_image, :secret, :permalink, :downloadlink, :playbackcount
   has_many :tracks
 end
